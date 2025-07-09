38943c32365f721a468200860f9cc9cda83ca186
Najeeb <najeebshaik08@gmail.com>
2025-07-09 22:37:19 +0100

    Adding changes from another repository

---
+++ api_client.py
@@ -0,0 +1,41 @@
+import requests
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+def fetch_jobs(pages=20):
+    base_url = "http://api.adzuna.com/v1/api/jobs/gb/search/"
+    all_jobs = []
+
+    for page in range(1, pages + 1):
+        url = f"{base_url}{page}"
+        params = {
+            "app_id": os.getenv("ADZUNA_APP_ID"),
+            "app_key": os.getenv("ADZUNA_APP_KEY"),
+            "what": "data engineer",
+            "results_per_page": 50
+        }
+
+        try:
+            response = requests.get(url, params=params, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+            
+            if 'results' in data:
+                all_jobs.extend(data['results'])
+                print(f"Fetched page {page} with {len(data['results'])} jobs.")
+            else:
+                print(f"No results on page {page}.")
+        
+        except requests.exceptions.RequestException as req_err:
+            print(f"Request error on page {page}: {req_err}")
+        except ValueError as val_err:
+            print(f"JSON decode error on page {page}: {val_err}")
+        except Exception as e:
+            print(f"Unexpected error on page {page}: {e}")
+        
+        time.sleep(0.100)  # optional: avoid hitting rate limits
+
+    print(f"Total jobs fetched: {len(all_jobs)}")
+    return all_jobs
---
+++ utils.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import json
+from datetime import datetime
+
+def save_raw_data(jobs, source="adzuna", directory = "datasets/raw"):
+    os.makedirs(directory, exist_ok=True)
+    today = datetime.now().strftime("%Y%m%d")
+    filename = f"datasets/raw/{today}_{source}_jobs.json"
+    with open(filename, 'w') as f:
+        json.dump(jobs, f, indent=2)
+    print(f"Saved raw data to {filename}")
+    return filename
+
+def load_raw_data(filename):
+    with open(filename) as f:
+        return json.load(f)
+    
+def save_processed_data(processed_jobs, source="adzuna", directory = "datasets/processed"):
+    os.makedirs(directory, exist_ok=True)
+    today = datetime.now().strftime("%Y%m%d")
+    filename = f"datasets/processed/{today}_{source}_jobs.csv"
+    df = pd.DataFrame(processed_jobs)
+    df.to_csv(filename, index=False)
+    print(f"Saved processed data to {filename}")
+    return filename
